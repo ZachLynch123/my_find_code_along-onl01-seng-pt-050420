3eeb1e1f479a1470c30b16c2e359f92b3c226438
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-05-11 17:59:10 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,9 +2,10 @@ require 'pry'
 
 def my_find(collection)
   i = 0 
-  some_array = []
   while i < collection.length 
-  some_array << yield(collection[i])
+  return collection[i] if 
+  
+  yield(collection[i])
   i += 1
 end
 
